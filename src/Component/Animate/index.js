484dc007e9a883b5196c8482028cc7945b30614c
keji <tomitoyo.93@gmail.com>
2021-08-14 02:22:51 +0100

    commit

--- src/Component/Animate/index.js
+++ src/Component/Animate/index.js
@@ -1,41 +1,22 @@
 import { Images } from "./imageData";
 import {  Button } from "@chakra-ui/react";
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
-import React, {useState, useEffect, useCallback } from 'react';
+import React, { useEffect } from 'react';
 import { Section } from "./style";
 
 function Animate() {
 	const image = Images
 	const [index, setIndex] = React.useState(0);
-	const [touchPosition, setTouchPosition] = useState(null)
 
-	const next = useCallback(
-		(index) => {
-			const lastIndex = image.length - 1;
+	useEffect(() => {
+		const lastIndex = image.length - 1;
 		if (index < 0) {
 			setIndex(lastIndex);
 		}
 		if (index > lastIndex) {
 			setIndex(0);
 		}
-		},
-		[image],
-	)
-		
-	useEffect(() => {
-	}, [index, image,next]);
-
-const prev = useCallback(
-	(index) => {
-		let slider = setInterval(() => {
-			setIndex(index + 1);
-		}, 5000);
-		return () => {
-			clearInterval(slider);
-		};
-	},
-	[],
-)
+	}, [index, image]);
 
 	useEffect(() => {
 		let slider = setInterval(() => {
@@ -44,33 +25,7 @@ const prev = useCallback(
 		return () => {
 			clearInterval(slider);
 		};
-	}, [index, prev]);
-
-	const handleTouchStart = (e) => {
-		const touchDown = e.touches[0].clientX
-		setTouchPosition(touchDown)
-}
-
-const handleTouchMove = (e) => {
-	const touchDown = touchPosition
-
-	if(touchDown === null) {
-					return
-	}
-
-	const currentTouch = e.touches[0].clientX
-	const diff = touchDown - currentTouch
-
-	if (diff > 1) {
-					next()
-	}
-
-	if (diff < -1) {
-					prev()
-	}
-
-	setTouchPosition(null)
-}
+	}, [index]);
 
 	return (
 		<Section className="section">
@@ -90,8 +45,7 @@ const handleTouchMove = (e) => {
 					}
 
 					return (
-						<article className={position} key={id} onTouchStart={handleTouchStart}
-						onTouchMove={handleTouchMove}>
+						<article className={position} key={id}>
 							<img src={img} alt={alt} className="person-img" />
 						</article>
 					);
